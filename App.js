7327ff343e277e92983d4cbb6a634017feb2b26e
amandapmalta <amandapmalta@gmail.com>
2021-04-09 15:26:55 +0100

    THIRD TEST

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is the second test!</Text>
+      <Text>This is the THIRD test!</Text>
       <StatusBar style="auto" />
     </View>
   );
