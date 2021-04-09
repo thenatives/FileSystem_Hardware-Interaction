59ad28dea16c619186713f27d6a8ffff55939a51
thenatives <81929755+thenatives@users.noreply.github.com>
2021-04-09 15:05:03 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is a test!</Text>
+      <Text>This is the second test!</Text>
       <StatusBar style="auto" />
     </View>
   );
